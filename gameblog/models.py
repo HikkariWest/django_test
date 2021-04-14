5712dc921fe6e02337be689351612f4592dd7b19
Hikkari <gosha.troncraft@gmail.com>
2021-04-14 19:26:30 +0600

    Добавил слаг в модель и функцию для слага

--- gameblog/models.py
+++ gameblog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
 class Post(modes.Model):
@@ -13,9 +15,15 @@ class Post(modes.Model):
 	content = models.TextField(blank = True)
 	category = models.SmallIntegerField(choices = CATEGORIES, default = 0)
 	draft = models.BooleanField(default = True)
+	slug = models.SlugField(null = False, unique = True, blank = True)
 
 	class Meta:
 		ordering = ('id', 'draft')
 
 	def __str__(self):
 		return self.title
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			self.slug = slugify(self.title)
+		return super().save(*args, **kwargs)

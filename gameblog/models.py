75d2d658bf6268e6bfa0fcf63e2e438b393d92ef
Hikkari <gosha.troncraft@gmail.com>
2021-04-14 19:02:10 +0600

    Добавил модели с категориями

--- gameblog/models.py
+++ gameblog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(modes.Model):
+	CATEGORIES = [
+	(0, 'Все'),
+	(1, 'Игровые новости'),
+	(2, 'Технологии'),
+	(3, 'Софт'),
+	(4, 'Новости киноиндустрии'),
+	]
+	title = models.CharField(max_length = 200)
+	content = models.TextField(blank = True)
+	category = models.SmallIntegerField(choices = CATEGORIES, default = 0)
+	draft = models.BooleanField(default = True)
